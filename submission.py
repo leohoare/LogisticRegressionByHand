dbe8a9282db0763f9864b572c29f166bd9013acb
leo <leo.hoare@live.com>
2019-05-09 14:25:06 +1000

    final

--- submission.py
+++ submission.py
@@ -1,8 +1,14 @@
-
 import pandas as pd
 import numpy as np
 
-def logistic_regression(data, labels, weights, num_epochs, learning_rate): # do not change the heading of the function
+'''
+Logistic Regression Function using gradient descent
+data - X matrix, (n,i) where n is rows, i is number of variables
+labels - Y matrix (n,1)
+num_epochs - desired epochs
+learning rate - rate at which model learns
+'''
+def logistic_regression(data, labels, weights, num_epochs, learning_rate): 
     for epoch in range(num_epochs):
         z = weights[0] + np.dot(data, weights[1:])
         sig = 1 / (1 + np.exp(-z))
@@ -11,15 +17,16 @@ def logistic_regression(data, labels, weights, num_epochs, learning_rate): # do 
         weights[0] = weights[0]-learning_rate*loss.sum()
         weights[1:] = weights[1:]-learning_rate*dotprod
         if epoch%100 == 0:
-            print(weights)
+            print(f'epoch:{epoch} weights:{weights}')
     return weights
 
-# data_file='./asset/a'
-# raw_data = pd.read_csv(data_file, sep=',')
-# labels=raw_data['Label'].values
-# data=np.stack((raw_data['Col1'].values,raw_data['Col2'].values), axis=-1)
-# ## Fixed Parameters. Please do not change values of these parameters...
-# weights = np.zeros(3) # We compute the weight for the intercept as well...
-# num_epochs = 50000
-# learning_rate = 50e-5
-# coefficients = logistic_regression(data, labels, weights, num_epochs, learning_rate)
+data_file ='./data'
+raw_data = pd.read_csv(data_file, sep=',')
+labels = raw_data['Label'].values
+data = raw_data[['Col1','Col2']]
+weights = np.zeros(data.shape[1] + 1) # We compute the weight for the intercept as well...
+num_epochs = 20000
+learning_rate = 50e-5
+coefficients = logistic_regression(data, labels, weights, num_epochs, learning_rate)
+print("Training Complete")
+print(f"Coefficients: {coefficients}")
